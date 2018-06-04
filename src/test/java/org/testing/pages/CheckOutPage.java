4cb623c9b737de5baf394140761d4f65783785ee
Deepak Bhaskaran <dbhaskaran@dbhaskaran-ltm4.internal.salesforce.com>
2018-06-04 14:19:18 -0400

    Added travis yml and updated to latest dependencies

--- src/test/java/org/testing/pages/CheckOutPage.java
+++ src/test/java/org/testing/pages/CheckOutPage.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testing.commons.CommonTest;
 
-public class CheckOutPage extends CommonTest{
-	
+public class CheckOutPage extends CommonTest {
+
 	WebDriver driver;
 	public static final Logger log = Logger.getLogger(CheckOutPage.class.getName());
 
@@ -30,18 +30,18 @@ public class CheckOutPage extends CommonTest{
 	WebElement verificationCode;
 	@FindBy(className = "btn")
 	WebElement orderBtn;
-	
+
 	public CheckOutPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public boolean verifyPageTitle() throws IOException {
 		if (driver.getTitle().toString().contains(getObject().getProperty("checkOutPageTitle")))
 			return true;
 		return false;
 	}
-	
+
 	public void enterCheckOutDetails() throws InterruptedException {
 		log.info("Entering Checkout details..");
 		log.info("Entering email address in email field..");
@@ -58,10 +58,10 @@ public class CheckOutPage extends CommonTest{
 		cardName.sendKeys("Curious Tester");
 		log.info("Entering verification code in verification field..");
 		verificationCode.sendKeys("407");
-}
-	
+	}
+
 	public MenuPage placeOrder() throws InterruptedException {
-		log.info("CPlacing Order..");
+		log.info("Placing Order..");
 		orderBtn.click();
 		log.info("Clicking Place Order..");
 		return new MenuPage(driver);
